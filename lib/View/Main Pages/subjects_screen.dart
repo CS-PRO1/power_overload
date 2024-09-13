8906ed44d60e6b2a341b7c5933e94168283d1ee4
Uravity-PRO <csprocsgo@gmail.com>
2024-09-14 00:48:28 +0300

    subjects

--- lib/View/Main Pages/subjects_screen.dart
+++ lib/View/Main Pages/subjects_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:power_overload/View/Main%20Pages/lessons_screen.dart';
@@ -24,55 +26,64 @@ class SubjectsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: green100,
         appBar: MyAppBar(
           title: 'المواد',
           leading: AppBarPopupMenu(),
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: GridView.count(
-              childAspectRatio: .65,
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              crossAxisSpacing: 4,
-              mainAxisSpacing: 4,
-              children: List.generate(
-                  page_info.length, (index) => flowershape(context, index)),
-            )));
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: GridView.count(
+                childAspectRatio: 1,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 10,
+                children: List.generate(
+                    page_info.length, (index) => flowershape(context, index)),
+              )),
+        ));
   }
 
   Widget flowershape(BuildContext context, int index) {
     return InkWell(
-      child: Stack(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: green400,
-            ),
-          ),
-          RotatedBox(
-            quarterTurns: 1,
-            child: Container(
-              width: 100,
-              height: 100,
+      splashColor: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Stack(
+          children: [
+            Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
                 color: green400,
               ),
             ),
-          ),
-          Text(
-            page_info[index],
-            style: TextStyle(color: green100),
-          ),
-        ],
+            Transform.rotate(
+              angle: 45 * pi / 180,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: green300,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                page_info[index],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: green100,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       onTap: () {
-        //TODO Login Request Function
         Navigator.of(context)
             .push(MaterialPageRoute(builder: (context) => LessonsScreen()));
       },
